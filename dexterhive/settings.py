f94d312eeffb92c982b0555b87de7ae8fa9cbe2d
Anshu Singh <anshu.singh@hashedin.com>
2016-08-29 00:48:37 +0530

    Initial frontend setup

--- dexterhive/settings.py
+++ dexterhive/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -98,6 +98,21 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_FINDERS = (
+  'django.contrib.staticfiles.finders.FileSystemFinder',
+  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATICFILES_DIRS = (
+  STATIC_DIR,
+)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'trackme', 'static')
 
 WSGI_APPLICATION = 'dexterhive.wsgi.application'
 
