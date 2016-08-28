15c5068e1eeded6a44ecbc163ad1d74749f46cdd
Mohd Sheeraz <mohd.sheeraz@hashedin.com>
2016-08-29 00:12:11 +0530

    Added Google Login

--- dexterhive/core/views.py
+++ dexterhive/core/views.py
@@ -1,3 +1,8 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/home')
+    return render(request, 'index.html')
