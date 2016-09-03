ef65c7dc84d2a04e13eea7ba3a6d04180db07181
Anshu Singh <anshu.singh@hashedin.com>
2016-09-04 01:14:55 +0530

    Home page design

--- dexterhive/core/views.py
+++ dexterhive/core/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def home(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/home')
-    return render(request, 'index.html')
+    return render(request, 'home.html')
