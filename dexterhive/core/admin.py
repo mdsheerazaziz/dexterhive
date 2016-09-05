ebb3ba84fd1af8c53e3edc3b2b9cadd1641dcc94
Mohd Sheeraz <mohd.sheeraz@hashedin.com>
2016-09-05 17:44:08 +0530

    Changed the invites to invitees

--- dexterhive/core/admin.py
+++ dexterhive/core/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from dexterhive.core import models
 
 admin.site.register(models.UserProfile)
-admin.site.register(models.Invites)
+admin.site.register(models.Invitees)
