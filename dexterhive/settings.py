27d649d13b067a29e4c2ecd6e6c28320b7474498
Mohd Sheeraz <mohd.sheeraz@hashedin.com>
2016-10-31 04:06:10 +0530

    Added Static url correctly

--- dexterhive/settings.py
+++ dexterhive/settings.py
@@ -111,9 +111,8 @@ STATICFILES_DIRS = (
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "static", "dexterhive")
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/dexterhive'
-
 WSGI_APPLICATION = 'dexterhive.wsgi.application'
 
 # Database
